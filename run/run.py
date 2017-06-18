b1c321df99b318aa6ccdb116a72610dbd6c6d597
maanitm <me@maanit.me>
2017-06-18 19:35:38 -0400

    run - servo pins all converted using constants - py

--- run/run.py
+++ run/run.py
@@ -17,6 +17,7 @@ servo.set_servo(constants.rightMotorBackPin, 1500)
 # WARNING: Right side inverted!
 
 drive = 0
+
 turn = 0
 
 print 'Initialized Servos For %s' % constants.projectName
@@ -49,37 +50,37 @@ try:
                 move = int(1500+driveTotal)
                 move2 = int(1500-driveTotal)
 
-                servo.set_servo(24, roundup(move))
-                servo.set_servo(12, roundup(move))
-                servo.set_servo(11, roundup(move2))
-                servo.set_servo(26, roundup(move2))
+                servo.set_servo(constant.leftMotorFrontPin, roundup(move))
+                servo.set_servo(constant.leftMotorBackPin, roundup(move))
+                servo.set_servo(constant.rightMotorFrontPin, roundup(move2))
+                servo.set_servo(constant.rightMotorBackPin, roundup(move2))
 
                 if turnTotal > 0: #Turn Right
                     if driveTotal > 0:
                         turnVal = int(move - turnTotal)
                         print ("Move: ", move, "Turn: ", turnTotal, "Both: ", turnVal)
-			servo.set_servo(11, roundup(turnVal))
-                        servo.set_servo(26, roundup(turnVal))
+			servo.set_servo(constants.rightMotorFrontPin, roundup(turnVal))
+                        servo.set_servo(constants.rightMotorBackPin, roundup(turnVal))
 			print("Move: ", move, " turn: ", turnTotal)
                     if driveTotal < 0:
                         turnVal = int(move2 - turnTotal)
-                        servo.set_servo(11, roundup(turnVal))
-                        servo.set_servo(26, roundup(turnVal))
+                        servo.set_servo(constants.rightMotorFrontPin, roundup(turnVal))
+                        servo.set_servo(constants.rightMotorBackPin, roundup(turnVal))
                 if turnTotal < 0: #Turn Left
                     if driveTotal > 0:
                         turnVal = int(move + turnTotal)
-                        servo.set_servo(24, roundup(turnVal))
-                        servo.set_servo(12, roundup(turnVal))
+                        servo.set_servo(constants.leftMotorFrontPin, roundup(turnVal))
+                        servo.set_servo(constants.leftMotorBackPin, roundup(turnVal))
                     if driveTotal < 0:
                         turnVal = int(move - turnTotal)
-                        servo.set_servo(24, roundup(turnVal))
-                        servo.set_servo(12, roundup(turnVal))
+                        servo.set_servo(constants.leftMotorFrontPin, roundup(turnVal))
+                        servo.set_servo(constants.leftMotorBackPin, roundup(turnVal))
 		if j.get_button(15):
 		    driveVal = int(1500 - driveTotal)
-		    servo.set_servo(11, roundup(driveVal))
-                    servo.set_servo(26, roundup(driveVal))
-		    servo.set_servo(24, roundup(driveVal))
-                    servo.set_servo(12, roundup(driveVal))
+		    servo.set_servo(constants.rightMotorFrontPin, roundup(driveVal))
+                    servo.set_servo(constants.rightMotorBackPin, roundup(driveVal))
+		    servo.set_servo(constants.leftMotorFrontPin, roundup(driveVal))
+                    servo.set_servo(constants.leftMotorBackPin, roundup(driveVal))
 	    if j.get_button(16):
                 j.quit()
                 sys.exit()
