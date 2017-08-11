15fbb3088cf4cd30bc5a3cdfd6ebbc1824f5dcff
Maanit Madan <me@maanit.me>
2017-08-11 00:33:42 -0400

    address change

--- main.py
+++ main.py
@@ -6,5 +6,5 @@ from testing import motor_test
 print(const.projectName)
 
 # run methods from packages and files
-batteryEstimate.run('4986 Weathervane Drive, Johns Creek, GA', '11435 Frazier Fir Ln, Alpharetta, GA')
+batteryEstimate.run('4986 Weathervane Drive, Johns Creek, GA', '11585 Jones Bridge Rd Ste 500, Johns Creek, GA')
 # motor_test.run()
