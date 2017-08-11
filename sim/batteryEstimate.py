d7b086b8103c121619b5346be48c4ba49e3bea69
Maanit Madan <me@maanit.me>
2017-08-11 00:29:11 -0400

    crazy changes

--- sim/batteryEstimate.py
+++ sim/batteryEstimate.py
@@ -1,25 +1,35 @@
 import sys
 import const
-import googlemaps
-import geojsonio
-from mapbox import Directions
 from modules.navigation import mapping
+import geojsonio
+import json
 
-def run():
-    print "Battery Simulation"
+def run(start, end):
+    print("Battery Simulation")
 
     maps = mapping.Mapping()
 
-    coordinates = maps.routeCoordinates('4986 Weathervane Drive, Johns Creek, GA', '11585 Jones Bridge Rd Ste 500, Johns Creek, GA 30022')
+    routes = maps.getDirection(start, end).geojson()
+    geojsonio.display(json.dumps(routes['features'][0], indent = 0))
+    distanceMeters = maps.distanceBetweeenLocations(start, end)
+
+    print(str(distanceMeters / 1000) + "km . " + str(distanceMeters * 0.000621371) + " mi")
+
+    print("Press enter to continue calculations or ^C to exit")
+    res = input()
+
+    coordinates = maps.routeCoordinates(start, end)
     altitudes = maps.altitudesFromCoordinates(coordinates)
 
+    print(len(coordinates))
+
     i = 0
-    print "# | Coordinate | Distance | Elevation"
-    for i in range(len(altitudes)):
+    print("# | Coordinate | Distance | Elevation")
+    for i in range(len(coordinates)):
         distance = 0
         elevation = 0
         if i is not 0:
             distance = maps.distanceFromCoordinates(coordinates[i-1][0], coordinates[i-1][1], coordinates[i][0], coordinates[i][1])
             elevation = altitudes[i]['elevation'] - altitudes[i - 1]['elevation']
-        print str(i) + " " + str(coordinates[i]) + " " + str(distance) + " " + str(elevation)
+        print(str(i) + " " + str(coordinates[i]) + " " + str(distance) + " " + str(elevation))
         i += 1
