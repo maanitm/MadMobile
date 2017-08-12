9059a280ea3f8e430e712fcea55e26913b265b4c
Maanit Madan <me@maanit.me>
2017-08-12 12:46:59 -0400

    main fixed

--- main.py
+++ main.py
@@ -2,20 +2,11 @@
 import const
 from sim import batteryEstimate
 from testing import motor_test
-<<<<<<< Updated upstream
 from testing import sidewalkTracking
-=======
-# from testing import sidewalkTracking
->>>>>>> Stashed changes
 
 print(const.projectName)
 
 # run methods from packages and files
 # batteryEstimate.run('4986 Weathervane Drive, Johns Creek, GA', '11585 Jones Bridge Rd Ste 500, Johns Creek, GA')
 # motor_test.run()
-<<<<<<< Updated upstream
 # sidewalkTracking.run()
-=======
-# SIDEWALK ONLY RUNS ON PYTHON 2
-sidewalkTracking.run()
->>>>>>> Stashed changes
