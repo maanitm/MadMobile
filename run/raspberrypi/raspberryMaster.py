602db9b3b92fc37bfdc232807853d8b0e4d89094
Maanit Madan <me@maanit.me>
2017-10-05 23:21:14 +0000

    raspberry pi flask additions

--- run/raspberrypi/raspberryMaster.py
+++ run/raspberrypi/raspberryMaster.py
@@ -1,17 +1,14 @@
 import smbus
 import time
 import pygame
+from flask import Flask, render_template, request
+from os import sys
+
+app = Flask(__name__)
+
+stopped = False
 
 drive = 0
-
-# Initialise the pygame library
-pygame.init()
-
-# Connect to the first JoyStick
-j = pygame.joystick.Joystick(0)
-j.init()
-
-print 'Initialized Joystick : %s' % j.get_name()
 
 # for RPI version 1, use "bus = smbus.SMBus(0)"
 bus = smbus.SMBus(1)
@@ -19,26 +16,45 @@ bus = smbus.SMBus(1)
 # This is the address we setup in the Arduino Program
 address = 0x04
 
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/writeNumber/<int:value>')
 def writeNumber(value):
   bus.write_byte(address, value)
   # bus.write_byte_data(address, 0, value)
-  return -1
+  return index()
 
 def readNumber():
   number = bus.read_byte(address)
   # number = bus.read_byte_data(address, 1)
   return number
 
+@app.route('/stopDrive')
 def stopDrive():
+    global stopped
+    stopped = True
     writeNumber(0)
-    print("Stopping ...")
-    j.quit()
-    pygame.quit()
-    sys.exit()
+    print "Stopped ..."
 
+@app.route('/startDrive')
 def startDrive():
+    # Initialise the pygame library
+    pygame.init()
+
+    # Connect to the first JoyStick
+    j = pygame.joystick.Joystick(0)
+    j.init()
+
+    print 'Initialized Joystick : %s' % j.get_name()
+
+    global stopped
+    global drive
+    stopped = False
     try:
-        while True:
+        while not stopped:
+            print(stopped)
 	    events = pygame.event.get()
             for event in events:
                 if event.type == pygame.JOYAXISMOTION:
@@ -51,7 +67,7 @@ def startDrive():
 	        var = 128
 
 	    if not var and var is not 0:
-    	        continue
+    	        var = 0
 
 	    if var >= 75:
   	        writeNumber(var)
@@ -59,11 +75,22 @@ def startDrive():
   	        # sleep tenth second
   	        time.sleep(0.1)
             if j.get_button(3):
-	        writeNumber(130)
-	        print "RPI: Hi Arduino, I sent you ", 130
-	        time.sleep(0.1)
+	        toggleAuto()
 	    if j.get_button(16):
 		stopDrive()
     except KeyboardInterrupt:
         j.quit()#!/usr/bin/env python
         pygame.quit()
+
+    return render_template('index.html')
+
+@app.route('/toggleAuto')
+def toggleAuto():
+    writeNumber(130)
+    print "RPI: Hi Arduino, I sent you ", 130
+    time.sleep(0.1)
+    return render_template('index.html')
+
+if __name__ == '__main__':
+     app.run(debug=True, host='0.0.0.0')
+
