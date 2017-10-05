0ed6588ef6834bdded319fb131d2f5a7bafc204e
Maanit Madan <me@maanit.me>
2017-10-05 19:42:03 -0400

    platforms added to main

--- main.py
+++ main.py
@@ -1,12 +1,25 @@
 # import packages to run
 import const
-# from testing import navSimulation
-# from testing import sidewalkTracking
-from run.raspberrypi import raspberryMaster
+from os import sys
 
 print(const.projectName)
 
-# run methods from packages and files
-# navSimulation.run('4986 Weathervane Drive, Johns Creek, GA', '11585 Jones Bridge Rd Ste 500, Johns Creek, GA')
-# sidewalkTracking.run(0)
-raspberryMaster.startDrive()
+platform = sys.argv[1]
+
+# platforms:
+#   mac-test - MacBook Air Test
+#   ras-pi - Raspberry Pi Master
+
+if platform == "mac-test":
+    from testing import navSimulation
+    from testing import sidewalkTracking
+
+    navSimulation.run('4986 Weathervane Drive, Johns Creek, GA', '11585 Jones Bridge Rd Ste 500, Johns Creek, GA')
+    sidewalkTracking.run(0)
+
+elif platform == "ras-pi":
+    from run.raspberrypi import raspberryMaster
+    raspberryMaster.startDrive()
+
+else:
+    print("platforms: \n    mac-test - MacBook Air Test\n    ras-pi - Raspberry Pi Master")
