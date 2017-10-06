5e8831c673eb32dc051ccb5ea6fbb22e66096ee4
Maanit Madan <me@maanit.me>
2017-10-06 16:40:05 -0400

    numbering testing added

--- run/raspberrypi/raspberryMaster.py
+++ run/raspberrypi/raspberryMaster.py
@@ -40,27 +40,21 @@ def distance():
     time.sleep(0.00001)
     GPIO.output(TRIG, 0)
 
-    startCheck = 0
-    startCheck = time.time()
     skipped = False
+    print("1")
     while GPIO.input(ECHO) == 0:
+        print("2")
         a = 0
-        # print(time.time() - startCheck)
-        print(startCheck)
-        if int(time.time() - startCheck) > 3:
-            skipped = True
-            continue
+    print("3")
     time1 = time.time()
     while GPIO.input(ECHO) == 1:
-        if skipped:
-            continue
+        print("4")
         a = 1
-    if not skipped:
-        time2 = time.time()
-        during = time2 - time1
-        return during * 340 / 2 * 100
-    else:
-        return 3000
+    print("5")
+    time2 = time.time()
+    during = time2 - time1
+    print("6")
+    return during * 340 / 2 * 100
 
 def writeNumber(value):
   bus.write_byte(address, value)
