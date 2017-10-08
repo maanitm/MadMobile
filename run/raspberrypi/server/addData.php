35f8d0a25a30ab751dc25c4931ea1e9fc20a238c
Maanit Madan <me@maanit.me>
2017-10-08 12:40:13 -0400

    errors on

--- run/raspberrypi/server/addData.php
+++ run/raspberrypi/server/addData.php
@@ -1,7 +1,9 @@
 <?php
     // error_reporting(0);
-    error_reporting(E_ALL);
-    ini_set('display_errors', 'on');
+    error_reporting(-1); // reports all errors
+    ini_set("display_errors", "1"); // shows all errors
+    ini_set("log_errors", 1);
+    ini_set("error_log", "/tmp/php-error.log");
     include 'mysqlConnector.php';
 
     $conn = connect_to_mysql();
