96c279e40a75af10419067a08623fa27800670d9
Maanit Madan <me@maanit.me>
2017-10-08 12:03:48 -0400

    test server url

--- run/raspberrypi/server/db_config.php
+++ run/raspberrypi/server/db_config.php
@@ -2,5 +2,5 @@
 	define('DB_USER', "admin"); // db user
 	define('DB_PASSWORD', "maanit3022"); // db password (mention your db password here)
 	define('DB_DATABASE', "madmobile"); // database name
-	define('DB_SERVER', "localhost"); // db server
+	define('DB_SERVER', "10.0.1.32"); // db server
 ?>
