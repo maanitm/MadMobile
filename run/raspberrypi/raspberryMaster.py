ea974511e07121f0ee37840fccec11f4adf9c867
Maanit Madan <me@maanit.me>
2017-11-20 18:36:25 -0500

    send new value

--- run/raspberrypi/raspberryMaster.py
+++ run/raspberrypi/raspberryMaster.py
@@ -63,7 +63,7 @@ def distance():
 def writeNumber(value):
   print(address)
   print(value)
-  bus.write_byte_data(address, 0, 2)
+  bus.write_byte_data(address, 0, value)
   return value
 
 # read number through serial from arduino
