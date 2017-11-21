0f3bd2ee6a1262c267a97d0a6085f31a8c84def9
Maanit Madan <me@maanit.me>
2017-11-21 15:18:13 -0500

    turning complete

--- run/arduino/arduinoActuator/arduinoActuator.ino
+++ run/arduino/arduinoActuator/arduinoActuator.ino
@@ -6,21 +6,15 @@
 #define pwmPin 3
 #define dirPin 2
 
-int currentVal = -1;
-int changedVal = 0;
-int newVal = 0;
+int stickVal = 50;
+int actuatorVal = 0;
 
 void setup() {
   // put your setup code here, to run once:
   pinMode(pwmPin, OUTPUT);
   pinMode(dirPin, OUTPUT);
 
-  currentVal = -1;
   setActuatorValue(50);
-//  digitalWrite(dirPin, HIGH);
-//    analogWrite(pwmPin, 255);
-//    delay((300/100) * 25);
-//    analogWrite(pwmPin, 0);
 
   Serial.begin(SERIAL_ADDRESS);
   SPI.begin();
@@ -34,73 +28,40 @@ void setup() {
 }
 
 void setActuatorValue(int value) {
-//  if (value > 0) {
-//    digitalWrite(dirPin, HIGH);
-//    analogWrite(pwmPin, 255);
-////    Serial.println("A");
-//    delay((3000/1000) * (value*10));
-////    Serial.println("B");
-//    analogWrite(pwmPin, 0); 
-//  }
-//  else if (value < 0) {
-//    digitalWrite(dirPin, LOW);
-//    analogWrite(pwmPin, 255);
-//    delay((3000/1000) * (value * -10));
-//    analogWrite(pwmPin, 0); 
-//  }
   if (value >= 0 && value <= 100) {
-    if (value > 50) {
+    int movement = (value - actuatorVal);
+    if (movement > 0) {
       digitalWrite(dirPin, HIGH);
       analogWrite(pwmPin, 255);
-      delay((300/100) * 50);
+      delay((300/100) * movement/2);
       analogWrite(pwmPin, 0);
-      currentVal = 1;
     }
-    else if (value < 50) {
+    else if (movement < 0) {
       digitalWrite(dirPin, LOW);
       analogWrite(pwmPin, 255);
-      delay((300/100) * 50);
+      delay((300/100) * ((movement * -1)/2));
       analogWrite(pwmPin, 0);
-      currentVal = -1;
     }
-    else {
-      if (currentVal == -1) {
-        digitalWrite(dirPin, HIGH);
-        analogWrite(pwmPin, 255);
-        delay((300/100) * 50);
-        analogWrite(pwmPin, 0); 
-      }
-      else if (currentVal == 1) {
-        digitalWrite(dirPin, LOW);
-        analogWrite(pwmPin, 255);
-        delay((300/100) * 50);
-        analogWrite(pwmPin, 0); 
-      }
-      currentVal = 0;
-    }
+    
+    actuatorVal = value;
     delay(5);
   }
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-//  Serial.println(changedVal);
-//  changedVal = newVal - currentVal;
-//  Serial.println("A");
-//  setActuatorValue(changedVal);
-//  Serial.println("B");
-//  currentVal = newVal;
-  setActuatorValue(newVal);
+  setActuatorValue(stickVal);
 }
 
 // callback for received data
 void receiveData(int byteCount) {
   while(Wire.available()) {
-    newVal = Wire.read();
+    stickVal = Wire.read();
+    delay(5);
   }
 }
 
 // callback for sending data
 void sendData() {
-  Wire.write(currentVal);
+  Wire.write(actuatorVal);
 }
