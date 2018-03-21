45df6459a6600dcec616c61eac69055cf25254a6
Maanit Madan <me@maanit.me>
2018-03-20 23:31:55 -0400

    turn working without ras-pi rn

--- run/arduino/arduinoActuator/arduinoActuator.ino
+++ run/arduino/arduinoActuator/arduinoActuator.ino
@@ -3,8 +3,14 @@
 #define DIR_PIN 2
 #define POT_PIN A0
 
+#define LEFT 455
+#define RIGHT 755
+
 int stickVal = 50;
 int actuatorVal = 0;
+int setpoint = 0;
+
+int testPoints[] = {10, 40, 80, 70, 90, 30, 0, 8, 32, 80};
 
 void setup() {
   // put your setup code here, to run once:
@@ -12,38 +18,44 @@ void setup() {
   pinMode(DIR_PIN, OUTPUT);
 
   actuatorVal = analogRead(POT_PIN);
-  actuatorVal = actuatorVal * (100/1023);
+  
+  actuatorVal = int((actuatorVal - LEFT)/3);
+
+  setpoint = actuatorVal;
 
   Serial.begin(SERIAL_ADDRESS);
-  
 
   Serial.println("Ready!");
+  Serial.println(actuatorVal);
 }
 
 void loop() {
   actuatorVal = analogRead(POT_PIN);
-  actuatorVal = actuatorVal * (100/1023);
+  actuatorVal = int((actuatorVal - LEFT)/3);
   
   if (Serial.available()) {
-    int setpoint = Serial.read() - '0';
-    
-    if (setpoint != actuatorVal) {
-      if (setpoint > actuatorVal) {
-        digitalWrite(DIR_PIN, HIGH);
-        while (actuatorVal < setpoint) {
-          actuatorVal = analogRead(POT_PIN);
-          actuatorVal = actuatorVal * (100/1023);
-          analogWrite(PWM_PIN, 255);
-        }
-      } else if (setpoint < actuatorVal) {
-        digitalWrite(DIR_PIN, LOW);
-        while (actuatorVal > setpoint) {
-          actuatorVal = analogRead(POT_PIN);
-          actuatorVal = actuatorVal * (100/1023);
-          analogWrite(PWM_PIN, 255);
-        }
-      }
-      analogWrite(PWM_PIN, 0);
-    }
+    setpoint = Serial.read() - '0';
+
+    Serial.println(Serial.read());
+
+    Serial.println("Setpoint: ");
+    Serial.println(setpoint);
+
+    Serial.println("ActuatorVal: ");
+    Serial.println(actuatorVal);
   }
+
+  if (setpoint != actuatorVal) {
+      if (actuatorVal < setpoint - 10) {
+        digitalWrite(DIR_PIN, LOW);
+        analogWrite(PWM_PIN, 255);
+      } else if (actuatorVal > setpoint + 10) {
+        digitalWrite(DIR_PIN, HIGH);
+        analogWrite(PWM_PIN, 255);
+      }
+  }
+  else {
+    analogWrite(PWM_PIN, 0);
+  }
+  
 }
