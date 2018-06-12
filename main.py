c170d6dab94f1a4770da49bef652b243f65ec371
Maanit Madan <me@maanit.me>
2018-06-12 10:05:27 -0400

    darknet added and sidewalk tracking testing

--- main.py
+++ main.py
@@ -11,11 +11,11 @@ platform = sys.argv[1]
 #   ras-pi - Raspberry Pi Master
 
 if platform == "mac-test":
-    from testing import navSimulation
+    # from testing import navSimulation
     from testing import sidewalkTracking
 
-    navSimulation.run('4986 Weathervane Drive, Johns Creek, GA', '11585 Jones Bridge Rd Ste 500, Johns Creek, GA')
-    sidewalkTracking.run(0)
+    # navSimulation.run('4986 Weathervane Drive, Johns Creek, GA', '11585 Jones Bridge Rd Ste 500, Johns Creek, GA')
+    sidewalkTracking.run('solidWhiteRight.mp4')
 
 if platform == "win-test":
     from testing import navSimulation
@@ -29,4 +29,4 @@ elif platform == "ras-pi":
     raspberryMaster.startDrive()
 
 else:
-    print("platforms: \n    mac-test - MacBook Air Test\n    ras-pi - Raspberry Pi Master")
+    print("platforms: \n    mac-test - MacBook Air Test\n    ras-pi - Raspberry Pi Master\n    win-test - Windows PC Test")
